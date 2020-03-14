ada6651901f924d64005d6b78d24c201729254ba
jorgeearanda2 <jorgeearanda2@gmail.com>
2020-03-14 12:35:35 +0100

    Adds loop to print

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int counter = 0;
+		while(counter < 10) {
+			System.out.println("Hello World " + counter);
+			counter++;
+		}
 	}
 }
